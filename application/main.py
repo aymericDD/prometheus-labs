27840188ae98f8a09ece5118210d3ae5e86d3295
aymericDD <aymeric42680@gmail.com>
2019-01-09 18:39:08 +0000

    Lab: Use label

--- application/main.py
+++ application/main.py
@@ -1,27 +1,17 @@
 import http.server
-import random
-import time
 from prometheus_client import start_http_server
-from prometheus_client import Gauge
-from prometheus_client import Histogram
+from prometheus_client import Counter
 
-INPROGRESS = Gauge('hello_worlds_inprogress',
-                   'Number of Hello Worlds in progress')
-LAST = Gauge('hello_worlds_last_time_seconds',
-             'The last time a Hello world as served')
-
-LATENCY = Histogram('hello_world_latency_seconds',
-                    'Time for a request Hello World')
+REQUESTS = Counter('hello_worlds_total',
+                   'Hello worlds requested',
+                   labelnames=['path', 'method'])
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
-  @LATENCY.time()
   def do_GET(self):
-    INPROGRESS.inc()
+    REQUESTS.labels(self.path, self.command).inc()
     self.send_response(200)
     self.end_headers()
     self.wfile.write(b"Hello World")
-    LAST.set(time.time())
-    INPROGRESS.dec()
 
 if __name__ == "__main__":
    start_http_server(8000)
