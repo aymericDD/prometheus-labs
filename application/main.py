f3584975d3f77e68ad64d0b58a56f3faea4ce6bf
aymericDD <aymeric42680@gmail.com>
2019-01-09 16:00:24 +0000

    Lab: Use Gauge/Histograme

--- application/main.py
+++ application/main.py
@@ -1,25 +1,27 @@
 import http.server
 import random
+import time
 from prometheus_client import start_http_server
-from prometheus_client import Counter
+from prometheus_client import Gauge
+from prometheus_client import Histogram
 
-REQUESTS = Counter('hello_worlds_total', 'Hello Worlds requested.')
-EXCEPTIONS = Counter('hello_worlds_exceptions_total',
-                     'Exceptions serving Hello World')
-SALES = Counter('hello_worlds_sales_euro_total',
-                'Euros made serving Hello World.')
+INPROGRESS = Gauge('hello_worlds_inprogress',
+                   'Number of Hello Worlds in progress')
+LAST = Gauge('hello_worlds_last_time_seconds',
+             'The last time a Hello world as served')
+
+LATENCY = Histogram('hello_world_latency_seconds',
+                    'Time for a request Hello World')
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
-  @EXCEPTIONS.count_exceptions()
+  @LATENCY.time()
   def do_GET(self):
-    REQUESTS.inc() 
-#    if random.random() < 0.2:
-#      raise Exception
-    euros = random.random()
-    SALES.inc(euros)
+    INPROGRESS.inc()
     self.send_response(200)
     self.end_headers()
     self.wfile.write(b"Hello World")
+    LAST.set(time.time())
+    INPROGRESS.dec()
 
 if __name__ == "__main__":
    start_http_server(8000)
